d9c414944c3f48a7ccf40f0557a567784d0a95de
Rohannaa2m12 <kusnadis404@gmail.com>
2026-02-17 10:49:44 +0100

    Update main.java

--- main.java
+++ main.java
@@ -35,3 +35,40 @@ enum FlipOutcome {
     public String getLabel() { return label; }
     public String getEmoji() { return emoji; }
 
+    public static FlipOutcome fromCode(int c) {
+        return c == 0 ? HEADS : TAILS;
+    }
+
+    public static FlipOutcome random() {
+        return ThreadLocalRandom.current().nextBoolean() ? HEADS : TAILS;
+    }
+}
+
+enum GameTier {
+    PEASANT(0, "Peasant", BigDecimal.valueOf(0.01), BigDecimal.valueOf(0.5)),
+    DEGEN(1, "Degen", BigDecimal.valueOf(0.5), BigDecimal.valueOf(2)),
+    WHALE(2, "Whale", BigDecimal.valueOf(2), BigDecimal.valueOf(10)),
+    SATOSHI(3, "Satoshi", BigDecimal.valueOf(10), BigDecimal.valueOf(100));
+
+    private final int index;
+    private final String name;
+    private final BigDecimal minEth;
+    private final BigDecimal maxEth;
+
+    GameTier(int index, String name, BigDecimal minEth, BigDecimal maxEth) {
+        this.index = index;
+        this.name = name;
+        this.minEth = minEth;
+        this.maxEth = maxEth;
+    }
+
+    public int getIndex() { return index; }
+    public String getName() { return name; }
+    public BigDecimal getMinEth() { return minEth; }
+    public BigDecimal getMaxEth() { return maxEth; }
+
+    public static GameTier forWager(BigDecimal eth) {
+        for (GameTier t : values()) {
+            if (eth.compareTo(t.minEth) >= 0 && eth.compareTo(t.maxEth) < 0) return t;
+        }
+        return SATOSHI;
